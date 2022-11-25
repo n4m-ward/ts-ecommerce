2eaa714e0707430efd5f9d3e54d1dc437e7219bc
n4m-ward <gabrielphp00@gmail.com>
2022-11-25 19:36:22 -0300

    create migrations to base database

---
+++ database/migrations/001.CreateTableItem.ts
@@ -0,0 +1,25 @@
+import MigrationInterface from "../MigrationInterface";
+import Connection from "../../src/Infra/Database/Connection";
+
+export default class CreateTableItem implements MigrationInterface {
+
+    constructor(readonly connection: Connection) {
+
+    }
+
+    async up(): Promise<void> {
+        await this.connection.query(
+            `
+                    create table item (
+                    id_item SERIAL PRIMARY KEY,
+                    description VARCHAR(100) NOT NULL,
+                    price VARCHAR(50) NOT NULL
+                )`,
+            {}
+        )
+    }
+
+    async down(): Promise<void> {
+        await this.connection.query("drop table if exists item")
+    }
+}
